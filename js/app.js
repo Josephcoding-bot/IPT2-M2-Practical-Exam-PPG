02af4f245bee1a6289679047c61ca19f23ff9bbe
Nicole <2301110148@student.buksu.edu.ph>
2025-09-25 23:31:23 +0800

    Implemented ADD STUDENT function to populate student table

--- js/app.js
+++ js/app.js
@@ -1,27 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const addSubjectButton = document.getElementById("addSubject");
-  const subjectTable = document.getElementById("table-content");
+  const addStudentButton = document.getElementById("addStudentButton");
+  const studentTable = document.getElementById("table-content");
 
-  if (addSubjectButton && subjectTable) {
-    addSubjectButton.addEventListener("click", () => {
-      const subjectCode = document.getElementById("subjectCode").value.trim();
-      const subjectName = document.getElementById("subjectName").value.trim();
-      const units       = document.getElementById("units").value.trim();
+  if (addStudentButton && studentTable) {
+    addStudentButton.addEventListener("click", () => {
+      const idNumber   = document.getElementById("idNumber").value.trim();
+      const firstName  = document.getElementById("firstName").value.trim();
+      const middleName = document.getElementById("middleName").value.trim();
+      const lastName   = document.getElementById("lastName").value.trim();
 
-      if (!subjectCode || !subjectName || !units) {
-        alert("Subject Code, Name, and Units are required.");
+      if (!idNumber || !firstName || !lastName) {
+        alert("ID Number, Firstname and Lastname are required.");
         return;
       }
 
       const row = document.createElement("tr");
       row.innerHTML = `
-        <td>${subjectCode}</td>
-        <td>${subjectName}</td>
-        <td>${units}</td>
+        <td>${idNumber}</td>
+        <td>${firstName}</td>
+        <td>${middleName}</td>
+        <td>${lastName}</td>
       `;
-      subjectTable.appendChild(row);
+      studentTable.appendChild(row);
 
-      document.getElementById("subjectForm").reset();
+      document.getElementById("studentForm").reset();
 
     });
   }
